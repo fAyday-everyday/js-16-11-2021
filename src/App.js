b87b156ecf93a9569a2c2b8d4a1c94f85405b7d1
Kasymov Marsel <marselkasymov23@gmail.com>
2022-02-17 19:54:48 +0600

    jsd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Readsfas
         </a>
       </header>
     </div>
