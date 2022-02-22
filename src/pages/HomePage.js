a32af271ae326abc5b020b1b487cbd5d39f32f4b
fAyday-everyday <polkiju489@gmaill.com>
2022-02-22 19:57:07 +0600

    block added to HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,17 +6,13 @@ export default function HomePage() {
 
     const options = [{ label: 'Tab 1', value: '1' }, { label: 'Tab 2', value: '2' }]
 
+
     return (
         <div className="cont">
-          <div className="home">
-              <Link to='/about'><p>About Us</p></Link>
-              <Link to='/materialpage'>Material UI</Link>
-              <button>Mode</button>
-          </div>
-          <Tabs options={options} /> 
-          <div className="maincss">
-              <Link to='/main'>Main Page Test</Link>
-          </div>
+            <div>
+                <Tabs options={options} />
+                <div className='asd'></div>
+            </div> 
         </div>
     )
 }
