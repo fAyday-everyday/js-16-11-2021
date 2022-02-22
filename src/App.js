028c35cbc8d9eb658baa3179fbb5875f1a95f24b
fAyday-everyday <polkiju489@gmaill.com>
2022-02-22 20:29:42 +0600

    fixed issues

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import HomePage from "./pages/HomePage";
-
-
+// import { Route, Routes } from "react-router-dom";
+// import Header from "./components/Header";
+// import HomePage from "./pages/HomePage";
 
 function App() {
   return (
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,10 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Tabs from "../components/Tabs/Tabs";
 import './main.css'
 
 export default function HomePage() {
 
     const options = [{ label: 'Tab 1', value: '1' }, { label: 'Tab 2', value: '2' }]
-
 
     return (
         <div className="cont">
